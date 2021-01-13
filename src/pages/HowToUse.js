ec45fdf753316c6b791a61e91266e772d59959bf
MoonTahoe <alex@moonhighway.com>
2021-01-13 11:03:22 -0800

    Removes mdx.macro library

--- src/pages/HowToUse.js
+++ src/pages/HowToUse.js
@@ -1,16 +1,10 @@
-import { lazy, Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Centered, PrevNextBar } from "../ui";
-import { importMDX } from "mdx.macro";
-
-const HowToUseContent = lazy(() => importMDX("../book/How To Use.mdx"));
 
 export default function HowToUse() {
   return (
     <Centered>
-      <Suspense fallback={<div>Loading...</div>}>
-        <HowToUseContent />
-      </Suspense>
+      <h1>TODO: Insert MDX</h1>
       <PrevNextBar>
         <Link to="/TOC">Prev:Table of Contents</Link>
         <Link to="/overview">Next:Course Overview</Link>
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,16 +1,10 @@
-import { lazy, Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Centered, PrevNextBar } from "../ui";
-import { importMDX } from "mdx.macro";
-
-const OverviewContent = lazy(() => importMDX("../book/Overview.mdx"));
 
 export default function Overview() {
   return (
     <Centered>
-      <Suspense fallback={<div>Loading...</div>}>
-        <OverviewContent />
-      </Suspense>
+      <h1>TODO: Insert MDX</h1>
       <PrevNextBar>
         <Link to="/HowToUse">Prev: How to use this guide</Link>
         <Link to="/agenda/Hello World">Start "Hello World"</Link>
