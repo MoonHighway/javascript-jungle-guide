8f8b84a123ac48ed97ba3b805814162214dda933
eveporcello <eve@moonhighway.com>
2021-01-13 13:47:04 -0800

    Adds toc first draft

--- src/pages/TOC.js
+++ src/pages/TOC.js
@@ -1,23 +1,54 @@
 import { Link } from "react-router-dom";
 import { Centered, PrevNextBar } from "../ui";
 import courseData from "../book/manifest.json";
+<<<<<<< HEAD
 import { totalTime } from "../lib";
+=======
+
+const total = (topics = []) =>
+  topics.reduce((sum, val) => {
+    return val.length + sum;
+  }, 0);
+
+// send a section
+>>>>>>> Adds toc first draft
 
 export default function TOC() {
   return (
     <Centered>
       <h1>TOC</h1>
+<<<<<<< HEAD
       {courseData.agenda.map(block => (
         <div>
           <h2>
             {block.title} - {totalTime(block)} mins
+=======
+
+      {courseData.agenda.map(block => (
+        <div>
+          <h2>
+            {block.title} -{" "}
+            {block.length
+              ? block.length
+              : "different thing"}
+>>>>>>> Adds toc first draft
           </h2>
           <hr />
           <ul>
             {block.agenda
               ? block.agenda.map(item => (
+<<<<<<< HEAD
                   <li>
                     {item.title} - {totalTime(item)} mins
+=======
+                  <li key={item.title}>
+                    {item.title}
+                    {" - "}
+                    {item.length
+                      ? item.length
+                      : total(item.topics)}
+                    {" minutes"}
+>>>>>>> Adds toc first draft
                   </li>
                 ))
               : block.steps.map(step => <li>{step}</li>)}
