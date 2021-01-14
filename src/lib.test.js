72493b2705896007de02981a3655c285b1300eda
eveporcello <eve@moonhighway.com>
2021-01-13 18:32:40 -0800

    adds total time function

---
+++ src/lib.test.js
@@ -0,0 +1,19 @@
+import { totalTime } from "./lib";
+import { testAgenda, fullAgenda } from "./lib.test.data";
+
+describe("Totaling times", () => {
+  it("Returns zero when nothing is sent", () => {
+    expect(totalTime()).toEqual(0);
+  });
+  it("Gets the topic total from length", () => {
+    expect(
+      totalTime({ title: "install-vscode", length: 3 })
+    ).toEqual(3);
+  });
+  it("Totals an entire section", () => {
+    expect(totalTime(testAgenda)).toEqual(60);
+  });
+  it("Totals an entire class", () => {
+    expect(totalTime(fullAgenda)).toEqual(205);
+  });
+});
