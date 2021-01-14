d7b3ad7a86d7e864cfb9d081e027b4f91ba5be72
eveporcello <eve@moonhighway.com>
2021-01-14 08:21:46 -0800

    adds working TOC page with totalTime function

--- src/pages/TOC.js
+++ src/pages/TOC.js
@@ -1,38 +1,23 @@
 import { Link } from "react-router-dom";
 import { Centered, PrevNextBar } from "../ui";
 import courseData from "../book/manifest.json";
-
-const total = (topics = []) =>
-  topics.reduce((sum, val) => {
-    return val.length + sum;
-  }, 0);
-
-// send a section
+import { totalTime } from "../lib";
 
 export default function TOC() {
   return (
     <Centered>
       <h1>TOC</h1>
-
       {courseData.agenda.map(block => (
         <div>
           <h2>
-            {block.title} -{" "}
-            {block.length
-              ? block.length
-              : "different thing"}
+            {block.title} - {totalTime(block)} mins
           </h2>
           <hr />
           <ul>
             {block.agenda
               ? block.agenda.map(item => (
-                  <li key={item.title}>
-                    {item.title}
-                    {" - "}
-                    {item.length
-                      ? item.length
-                      : total(item.topics)}
-                    {" minutes"}
+                  <li>
+                    {item.title} - {totalTime(item)} mins
                   </li>
                 ))
               : block.steps.map(step => <li>{step}</li>)}
