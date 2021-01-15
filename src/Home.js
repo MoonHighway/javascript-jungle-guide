dce40700eba5aab47c767ce2900e7e5b8a29da77
eveporcello <eve@moonhighway.com>
2021-01-15 08:47:21 -0800

    adds images

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 import { Centered } from "./ui";
 import styled from "styled-components";
+import JSLogo from "./images/JSLogo.png";
+import NodeJSLogo from "./images/NodeJSLogo.png";
+import MHLogo from "./images/MHLogo.png";
+import PayPalLogo from "./images/PayPalLogo.png";
 
 export default function Home() {
   return (
     <Centered>
+      <img src={JSLogo} alt="JavaScript logo" />
+      <img src={NodeJSLogo} alt="Node.js logo" />
       <Title>Welcome to the JavaScript Jungle</Title>
       <Link to="/TOC">Start</Link>
+      <img src={MHLogo} alt="Moon Highway logo" />
+      <img src={PayPalLogo} alt="PayPal logo" />
     </Centered>
   );
 }
