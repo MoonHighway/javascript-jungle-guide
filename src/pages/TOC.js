c90c3d581be9870f0af371cf069c339dba6003d1
eveporcello <eve@moonhighway.com>
2021-01-19 18:56:15 -0800

    Adds Howtouse and overview pages

--- src/pages/TOC.js
+++ src/pages/TOC.js
@@ -2,32 +2,41 @@ import { Link } from "react-router-dom";
 import { Centered, PrevNextBar } from "../ui";
 import courseData from "../book/manifest.json";
 import { totalTime } from "../lib";
+import styled from "styled-components";
 
 export default function TOC() {
   return (
     <Centered>
-      <h1>TOC</h1>
-      {courseData.agenda.map((block) => (
-        <div>
-          <h2>
-            {block.title} - {totalTime(block)} mins
-          </h2>
-          <hr />
-          <ul>
-            {block.agenda
-              ? block.agenda.map((item) => (
-                  <li>
-                    {item.title} - {totalTime(item)} mins
-                  </li>
-                ))
-              : block.steps.map((step) => <li>{step}</li>)}
-          </ul>
-        </div>
-      ))}
-      <PrevNextBar>
-        <Link to="/">Prev:Cover</Link>
-        <Link to="/HowToUse">Next:How to use this guide</Link>
-      </PrevNextBar>
+      <Container>
+        <h1>TOC</h1>
+        {courseData.agenda.map(block => (
+          <div>
+            <h2>
+              {block.title} - {totalTime(block)} mins
+            </h2>
+            <hr />
+            <ul>
+              {block.agenda
+                ? block.agenda.map(item => (
+                    <li>
+                      {item.title} - {totalTime(item)} mins
+                    </li>
+                  ))
+                : block.steps.map(step => <li>{step}</li>)}
+            </ul>
+          </div>
+        ))}
+        <PrevNextBar>
+          <Link to="/">Prev:Cover</Link>
+          <Link to="/HowToUse">
+            Next:How to use this guide
+          </Link>
+        </PrevNextBar>
+      </Container>
     </Centered>
   );
 }
+
+const Container = styled.div`
+  width: 80%;
+`;
