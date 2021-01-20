794e5aa37c980323d3c413623ea40c806f9cac69
MoonTahoe <alex@moonhighway.com>
2021-01-19 16:36:39 -0800

    Next Prev thorugh intro slides

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -1,27 +1,56 @@
 import { lazy, Suspense } from "react";
 import data from "../book/manifest.json";
-import { useParams, Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { BookPage, PrevNextBar } from "../ui";
 
-const isSection = (routes = []) => routes.length === 1;
+const pathToAgenda = (path) =>
+  path.split("/").filter((r) => r && r !== "agenda");
+
+const toUrl = (...args) =>
+  args.map((a) => a.trim().replace(" ", "-").toLowerCase()).join("/");
+
+const checkTitleAgainst = (val) => (obj) =>
+  obj.title.toLowerCase() === val.replace("-", " ").toLowerCase();
+
+const findIndicesAllAround = (list, item) => {
+  const index = list.findIndex((i) => i.title === item.title);
+  return [
+    index <= list.length ? index + 1 : null,
+    index === 0 ? null : index - 1,
+  ];
+};
 
 function useBookContent(path) {
-  const routes = path.split("/");
-  if (isSection(routes)) {
-    const [route] = routes;
-    const agenda = data.agenda.find(
-      (agenda) =>
-        agenda.title.toLowerCase() === route.replace("-", " ").toLowerCase()
-    );
+  const routes = pathToAgenda(path);
+  const [sectionName, topicName, subTopicName, stepName] = routes;
+  const agenda = data.agenda.find(checkTitleAgainst(sectionName));
+  const topic = topicName && agenda.agenda.find(checkTitleAgainst(topicName));
+
+  if (!topicName) {
+    const [topic] = agenda.agenda;
     return {
-      title: agenda.title,
       fileName: `${agenda.title}/README.md`,
       previous: {
         title: "Course Overview",
         route: "/Overview",
       },
       next: {
-        title: "TODO",
+        title: topic.title,
+        route: `/agenda/${toUrl(agenda.title, topic.title)}`,
+      },
+    };
+  }
+
+  if (!subTopicName) {
+    return {
+      title: "Holder",
+      fileName: `${agenda.title}/${topic.title}/README.md`,
+      previous: {
+        title: agenda.title,
+        route: `/agenda/${toUrl(agenda.title)}`,
+      },
+      next: {
+        title: "HOLD",
         route: "",
       },
     };
@@ -29,8 +58,8 @@ function useBookContent(path) {
 }
 
 export default function Agenda() {
-  let { path } = useParams();
-  const { fileName, previous, next } = useBookContent(path);
+  let { pathname } = useLocation();
+  const { fileName, previous, next } = useBookContent(pathname);
 
   const Content = lazy(() =>
     import(`!babel-loader!mdx-loader!../book/${fileName}`)
